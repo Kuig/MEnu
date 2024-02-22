dad5527d22fb1b2f81e87d565a7685a41ded9334
Giorgio Presti <giorgio.presti@gmail.com>
2024-02-22 17:18:13 +0100

    Set this thing free

---
+++ MEnu.py
@@ -0,0 +1,156 @@
+import pandas as pd
+import datetime as dt
+from numpy import random
+
+# -----------------------------------------------------
+
+# MEnu :: A weekly menu planner by Giorgio Presti
+
+# -----------------------------------------------------
+
+__version__ = "1.1.0"
+
+### MAIN VARIABLES ###
+
+settings = pd.DataFrame({'Name_0':['Alice'],'Name_1':['Bob'], 'Dedication':['Potato']})
+
+portate = pd.DataFrame({'Nome':[],'Contenuto':[]})
+
+piatti = pd.DataFrame({'Nome':[],'Tipo':[], 'Stagione':[], 'Kids':[], 'Giorgio':[]})
+
+
+
+### FUNCTIONS ###
+
+# Load data and settings from excel file
+# Portate: a list of dish types
+# Piatti: a list of all available dishes
+def loadData(dbFile = 'Menu.xlsx'):
+    portate = pd.read_excel(dbFile, 'Portate')
+    piatti = pd.read_excel(dbFile, 'Piatti')
+    settings = pd.read_excel(dbFile, 'Settings').set_index('Variable').T
+    return [portate, piatti, settings]
+
+# Takes a list of Portate and a flag telling the sampler if it is the case to omit pizza.
+# Returns a random Portata and a flag which is true if it is pizza time.
+# The special case for pizza is due to my strong belief that at least one pizza a week is necessary, 
+# but at the same time my companion strongly belief one pizza a week is enough.
+# You can use the pizza type (z) to flag something you just want once a week.
+def getPortata(portate = pd.DataFrame({'Nome':[],'Contenuto':[]}), 
+             portataPrecedente = pd.DataFrame({'Nome':['no'],'Contenuto':['x']}),
+             giaPizzato = False,
+             seed = random.default_rng()):
+    
+    maschera = [(item != 'z' or not giaPizzato) for item in portate.Contenuto.to_list()]
+    disponibili = portate[(portate.Nome != portataPrecedente.Nome.to_list()[0]) & maschera]
+    r = disponibili.sample(random_state=seed)
+
+    return [r, giaPizzato or r.Contenuto.to_list()[0] == 'z']
+
+# Returns a dish given a list of dishes, the desired Portata, and some constrain
+# (season, presence of person_1, presence of person_2, random seed, meal id / counter)
+# Also returns the dish that needs to be dropped from the list
+def getPiatto(piatti = pd.DataFrame({'Nome':[],'Tipo':[], 'Stagione':[], settings.Name_0[0]:[], settings.Name_1[0]:[]}), 
+              portata = pd.DataFrame({'Nome':[],'Contenuto':[]}), 
+              stagione = 1, 
+              presName0 = 0, 
+              presName1 = 0,
+              seed = random.default_rng(),
+              id = 0):
+
+    portateDaPescare = [*portata.Contenuto.to_list()[0]]
+
+    disponibili = piatti[(piatti.Stagione != -stagione) & 
+                         (piatti[settings.Name_0[0]] >= presName0) & 
+                         (piatti[settings.Name_1[0]] >= presName1) &
+                         (piatti.Cena != -((id % 2)*2-1))]
+    
+    r = []
+    toBeDropped = []
+
+    for t in portateDaPescare:
+        sampled = disponibili[disponibili.Tipo == t].sample(random_state=seed)
+
+        if (t == 't' or t == 'p' or t == 'u' or t == 'z'): # TODO - Turn this into list-based, picked from settings sheet in the excel file
+            toBeDropped.append(sampled.index[0])
+
+        r.append(sampled.Nome.to_list()[0])
+    
+    return [r, toBeDropped]
+
+# Returns a random meal from a list of Portate and dishes, considering usual constrain of getPiatto
+# (this is basically like calling getPiatto over the result of getPortata)
+def getPasto(portate = pd.DataFrame({'Nome':[],'Contenuto':[]}), 
+                piatti = pd.DataFrame({'Nome':[],'Tipo':[], 'Stagione':[], settings.Name_0[0]:[], settings.Name_1[0]:[]}),
+                portataPrecedente = pd.DataFrame({'Nome':['no'],'Contenuto':['x']}),
+                giaPizzato = False,
+                stagione = 1, 
+                presName0 = 0, 
+                presName1 = 0,
+                seed = random.default_rng(),
+                id = 0):
+    
+    [p, giaPizzato] = getPortata(portate, portataPrecedente, giaPizzato, seed)
+    [r, toBeDropped] = getPiatto(piatti, p, stagione, presName0, presName1, seed, id)
+
+    return [r, p, giaPizzato, toBeDropped]
+
+# Returns a table with the weekly menu
+def getWeek(portate = pd.DataFrame({'Nome':[],'Contenuto':[]}), 
+            piatti = pd.DataFrame({'Nome':[],'Tipo':[], 'Stagione':[], settings.Name_0[0]:[], settings.Name_1[0]:[]}),
+            inputTable = {},
+            stagione = -1,
+            weekId = 0):
+    
+    p = pd.DataFrame({'Nome':['no'],'Contenuto':['x']})
+    giaPizzato = False
+
+    outputTable = {
+        'portata': ['No','No','No','No','No','No','No','No','No','No','No','No','No','No'],
+        'piatto': ['','','','','','','','','','','','','','']
+    }
+
+    seed = random.default_rng(weekId)
+
+    for port in range(14):
+        [r, p, giaPizzato, toBeDropped] = getPasto(portate, piatti, p, giaPizzato, stagione, inputTable['Name_0'][port], inputTable['Name_1'][port], seed, port)
+        outputTable['portata'][port] = p.Nome.to_list()[0]
+        outputTable['piatto'][port] = ', '.join(r)
+        piatti = piatti.drop(toBeDropped, axis='index')
+
+    return outputTable
+
+
+
+### STATE VARIABLES ###
+
+labelTable = {
+    'name': ['Pranzo','Cena','Pranzo','Cena','Pranzo','Cena','Pranzo','Cena','Pranzo','Cena','Pranzo','Cena','Pranzo','Cena'],
+    'day':  ['Lunedì','Lunedì','Martedì','Martedì','Mercoledì','Mercoledì','Giovedì','Giovedì','Venerdì','Venerdì','Sabato','Sabato','Domenica','Domenica']
+}
+
+inputTable = {
+    'Name_0': [0,1,0,1,0,1,0,1,0,1,1,1,1,1],
+    'Name_1': [1,1,0,1,0,1,0,1,0,1,1,1,1,1]
+}
+
+# Init with current week number as seed
+weekId = dt.date.today().isocalendar()[1]
+stagione = 1 if (weekId > 14 and weekId < 43) else -1
+
+
+
+### MAIN ###
+
+if __name__ == '__main__':
+    [portate, piatti, settings] = loadData(dbFile = 'Menu.xlsx')
+
+    outputTable = getWeek(portate, piatti, inputTable, stagione, weekId)
+    
+    print('Week ' + str(weekId) + ':')
+    
+    for e in range(14):
+        print(e.__str__().ljust(4) + 
+            (labelTable['day'][e] + ' a ' + labelTable['name'][e] + ':').ljust(21) +
+            outputTable['portata'][e].ljust(15) +
+            outputTable['piatto'][e])
---
+++ Menu_gui.py
@@ -0,0 +1,178 @@
+import MEnu as me
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from PIL import ImageGrab
+import ctypes
+
+# This helps tkinter and the image grabber to be aware of OS display scaling (under Windows)
+# Feel free to remove these lines if the "save" function produces weird pictures.
+ctypes.windll.shcore.SetProcessDpiAwareness(2)
+#ctypes.windll.user32.SetProcessDPIAware()
+
+# Some layout parameters
+_padx = 10
+_pady = 2
+_ckPadXMul = 2.1
+_tabPady = 20
+_wrapLen = 80
+_boldFont = 'Arial 9 bold'
+
+# GUI mainproperties
+window = tk.Tk()
+window.geometry("750x330")
+window.title(me.settings.Dedication[0] + " MEnu! - Ver. " + me.__version__)
+window.resizable(False, False)
+
+lunchIdVar = tk.StringVar(window)
+lunchIdVar.set(str(me.weekId))
+
+# A class containing a matrix of checkboxes
+class Checkbar(tk.Frame):
+    def __init__(self, parent=None, picks=[], r=0, c=0):
+        tk.Frame.__init__(self, parent)
+        self.vars = []
+        for p, pick in enumerate(picks):
+            var = tk.IntVar()
+            chk = tk.Checkbutton(self, text=pick, variable=var, command=inputTableChanged)
+            chk.grid(row=r+p%2, column=c+p//2, padx=_padx*_ckPadXMul, pady=_pady)
+            self.vars.append(var)
+    def state(self):
+        return [m for m in map((lambda var: var.get()), self.vars)]
+    def setState(self, newState):
+        for v, var in enumerate(self.vars):
+            var.set(newState[v])
+
+# A class containing a matrix of labels (for menù output)
+class PasTable(tk.Frame):
+    def __init__(self, parent=None):
+        tk.Frame.__init__(self, parent)
+        self.cells = [[tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self)],
+             [tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self)],
+             [tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self),tk.Label(self)]]
+        day_row = ['Lunedì','Martedì','Mercoledì','Giovedì','Venerdì','Sabato','Domenica']
+        for d in range(7):
+            self.cells[0][d+1].config(text=day_row[d],font=_boldFont)
+            self.cells[0][d+1].grid(row=0, column=d+1, padx=_padx, pady=_tabPady)
+    
+        self.cells[1][0].config(text='Pranzo',font=_boldFont)
+        self.cells[2][0].config(text='Cena',font=_boldFont)
+        self.cells[1][0].grid(row=1, column=0, padx=_padx, pady=_pady, sticky='E')
+        self.cells[2][0].grid(row=2, column=0, padx=_padx, pady=_pady, sticky='E')
+
+    def displayMenu(self):
+        piatti = me.outputTable['piatto']
+        for p in range(14):
+            cr = 1 + p % 2
+            cc = 1 + p // 2
+            self.cells[cr][cc].config(text=piatti[p], wraplength=_wrapLen, justify='center')
+            self.cells[cr][cc].grid(row=cr, column=cc, padx=_padx, pady=_pady)
+
+# Update menù based on settings 
+#(reloads data every time... slow, but effective when live editing the excel file)
+def updateWeekMenu(*args):
+    me.stagione = seasonCombo.current() * 2 - 1
+    me.weekId = int(lunchIdVar.get(), base=10)
+    [me.portate, me.piatti, me.settings] = me.loadData(dbFile = 'Menu.xlsx')
+    me.outputTable = me.getWeek(me.portate, me.piatti, me.inputTable, me.stagione, me.weekId)
+    menuTab.displayMenu()
+
+# Checkbox matrix callback
+def inputTableChanged():
+    me.inputTable['Name_0'] = p0PresCheck.state()
+    me.inputTable['Name_1'] = p1PresCheck.state()
+    updateWeekMenu()
+
+# Helper function that print static text around the GUI
+def genStaticText():
+    text = "ID settimana:"
+    text_output1 = tk.Label(window, text=text)
+    text_output1.grid(row=1, column=0, padx=_padx, sticky="W")
+    #, fg="red", font=("Helvetica", 16)
+
+    text = "Stagione:"
+    text_output2 = tk.Label(window, text=text)
+    text_output2.grid(row=3, column=0, padx=_padx, sticky="W")
+
+    text = "Presenze:"
+    text_output3 = tk.Label(window, text=text)
+    text_output3.grid(row=0, column=1, padx=_padx, pady=_pady, sticky="E")
+
+    text = me.settings.Name_0[0] + " Pranzo:"
+    text_output4 = tk.Label(window, text=text)
+    text_output4.grid(row=1, column=1, padx=_padx, pady=_pady, sticky="E")
+
+    text = me.settings.Name_0[0] + " Cena:"
+    text_output5 = tk.Label(window, text=text)
+    text_output5.grid(row=2, column=1, padx=_padx, pady=_pady, sticky="E")
+
+    text = me.settings.Name_1[0] + " Pranzo:"
+    text_output6 = tk.Label(window, text=text)
+    text_output6.grid(row=3, column=1, padx=_padx, pady=_pady, sticky="E")
+
+    text = me.settings.Name_1[0] + " Cena:"
+    text_output6 = tk.Label(window, text=text)
+    text_output6.grid(row=4, column=1, padx=_padx, pady=_pady, sticky="E")
+
+    day = ['LUN','MAR','MER','GIO','VEN','SAB','DOM']
+    day_t = [tk.Label(window),tk.Label(window),tk.Label(window),tk.Label(window),tk.Label(window),tk.Label(window),tk.Label(window)]
+
+    for l in range(7):
+        day_t[l].config(text=day[l])
+        day_t[l].grid(row=0, column=2+l, padx=_padx*2, pady=_pady)
+
+# Takes a snapshot of the window and saves it as PNG
+# Default filename includes current WeekID
+def capture_window():
+    x = window.winfo_rootx()
+    y = window.winfo_rooty()
+    width = window.winfo_width()
+    height = window.winfo_height()
+    takescreenshot = ImageGrab.grab(bbox=(x, y, x+width, y+height))
+    f = fd.asksaveasfile(initialfile = 'Menu_'+str(me.weekId)+'.png',
+                         defaultextension=".png",filetypes=[("Portable Network Graphics","*.png")])
+    if f:
+        f.close()
+        takescreenshot.save(f.name)
+
+# Week ID
+lunchIdSpin = tk.Spinbox(
+    window,
+    textvariable=lunchIdVar,
+    from_= 1,
+    to= 9999,
+    wrap=True,
+    command=updateWeekMenu
+)
+
+# Season
+seasonCombo = ttk.Combobox(
+    state="readonly",
+    values=["Fa freddo", "Fa caldo"]
+)
+
+# Other GUI elements
+p0PresCheck = Checkbar(window, ['', '', '', '', '', '', '', '', '', '', '', '', '', ''])
+p0PresCheck.setState(me.inputTable['Name_0'])
+p1PresCheck = Checkbar(window, ['', '', '', '', '', '', '', '', '', '', '', '', '', ''])
+p1PresCheck.setState(me.inputTable['Name_1'])
+menuTab = PasTable(window)
+
+# Layout
+genStaticText()
+saveBtn = tk.Button(window, text="Salva...", command=capture_window)
+saveBtn.grid(row=0, column=0, padx=_padx, pady=_pady, sticky="EW")
+lunchIdSpin.grid(row=2, column=0, padx=_padx, pady=_pady, sticky="W")
+seasonCombo.grid(row=4, column=0, padx=_padx, pady=_pady, sticky="W")
+seasonCombo.bind("<<ComboboxSelected>>", updateWeekMenu)
+seasonCombo.current((me.stagione + 1) // 2)
+p0PresCheck.grid(row=1, column=2, rowspan=2, columnspan=7)
+p1PresCheck.grid(row=3, column=2, rowspan=2, columnspan=7)
+menuTab.grid(row=5, column=0, rowspan=4, columnspan=9)
+
+# Init the main thing
+updateWeekMenu()
+
+# Hello, I'm the GUI
+if __name__ == "__main__":
+    window.mainloop()
